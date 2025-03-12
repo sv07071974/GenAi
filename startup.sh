58eb7a826a724df67aabbfdb8fe8e574557b5f84
sv07071974 <79800573+sv07071974@users.noreply.github.com>
2025-03-12 08:24:12 +0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-export PYTHONUNBUFFERED=1
-echo "Starting application..." > /home/LogFiles/startup.log 2>&1
+echo '#!/bin/bash
 cd /home/site/wwwroot
-gunicorn --bind=0.0.0.0:8000 --timeout 600 --workers=1 app:app >> /home/LogFiles/gunicorn.log 2>&1
+export PYTHONUNBUFFERED=1
+python -m gunicorn --bind=0.0.0.0:8000 --timeout 600 --workers=1 app:app' > startup.sh
