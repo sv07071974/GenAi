132cc8c0ac8e666d8f613cf5f8dc96f97a99c964
sv07071974 <79800573+sv07071974@users.noreply.github.com>
2025-03-13 14:12:32 +0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Start Gunicorn with appropriate settings for Azure
-gunicorn --bind=0.0.0.0:8000 --timeout 600 --workers 1 --threads 8 wsgi:application
+gunicorn --bind=0.0.0.0:8000 --timeout 600 --workers 1 --threads 8 app:app
