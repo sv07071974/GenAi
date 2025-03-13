bf170cb416dba99fccf9006bcf673c2504c2bb38
sv07071974 <79800573+sv07071974@users.noreply.github.com>
2025-03-13 17:50:13 +0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-# Start Gunicorn with appropriate settings for Azure
-gunicorn --bind=0.0.0.0:8000 --timeout 600 --workers 1 --threads 8 app:app
+# Create necessary directories
+mkdir -p uploads logs
